676ec698b30ec1f9523e4a4bfe4f7da25120ef25
Noel Kim <76243689+crlotwhite@users.noreply.github.com>
2025-06-20 22:31:59 +0900

    Update alignment module __init__.py

--- pytorch_hmm/alignment/__init__.py
+++ pytorch_hmm/alignment/__init__.py
@@ -14,12 +14,42 @@ from .dtw import (
     DTWAligner,
     dtw_alignment,
     compute_dtw_path,
-    dtw_distance
+    dtw_distance,
+    ConstrainedDTWAligner,
+    phoneme_audio_alignment,
+    extract_phoneme_durations
+)
+
+from .ctc import (
+    CTCAligner,
+    CTCSegmentationAligner,
+    ctc_alignment_path,
+    expand_targets_with_blank,
+    ctc_forward_algorithm,
+    ctc_backward_algorithm,
+    remove_ctc_blanks,
+    collapse_repeated_tokens,
+    ctc_decode_sequence
 )
 
 __all__ = [
+    # DTW alignment
     "DTWAligner",
     "dtw_alignment", 
     "compute_dtw_path",
-    "dtw_distance"
+    "dtw_distance",
+    "ConstrainedDTWAligner",
+    "phoneme_audio_alignment",
+    "extract_phoneme_durations",
+    
+    # CTC alignment
+    "CTCAligner",
+    "CTCSegmentationAligner", 
+    "ctc_alignment_path",
+    "expand_targets_with_blank",
+    "ctc_forward_algorithm",
+    "ctc_backward_algorithm",
+    "remove_ctc_blanks",
+    "collapse_repeated_tokens",
+    "ctc_decode_sequence"
 ]
